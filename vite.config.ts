4a3344250ad3538946a27a0a521797089fa26628
Marissa! 3 <marimarimarissa.go@gmail.com>
2024-11-11 23:17:11 -0700

    Moved index back out

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: "/",
-  root: 'src',
+  root: './',
   build: {
     outDir: '../dist',
     emptyOutDir: true, // also necessary
