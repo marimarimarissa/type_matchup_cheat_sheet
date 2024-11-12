fe5079fec25a9b7766752c58cb7f4d5593262cf5
Marissa! 3 <marimarimarissa.go@gmail.com>
2024-11-11 22:40:14 -0700

    Updated build, works in preview

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://myusername.github.io/type_matchup_cheat_sheet/"
+  base: "/",
+  root: 'src',
+  build: {
+    outDir: '../dist',
+    emptyOutDir: true, // also necessary
+  }
 })
